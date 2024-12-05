8c1ce17667eadd0bd104a6725d508061565ef76d
FogasM <wayford95@gmail.com>
2024-12-05 18:19:25 +0100

    Eljarassa alakitva

--- teszt2.py
+++ teszt2.py
@@ -1,8 +1,10 @@
 import random
 
-Rszamok = []
 
-for i in range(20):
-    szamok = random.randrange(1,50)
-    Rszamok.append(szamok)
-print(Rszamok)
+def veletlen():
+    Rszamok = []
+    for i in range(20):
+        Rszamok.append(random.randrange(1,50))
+    print(Rszamok)
+
+veletlen()
